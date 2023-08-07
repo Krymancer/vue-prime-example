4b74d0dab5c950f54399983ed770e39bd20797a0
Júnior Nascimento <junior.nascm@gmail.com>
2023-08-07 13:40:13 -0300

    init

--- src/stores/employeeStore.ts
+++ src/stores/employeeStore.ts
@@ -1,15 +1,11 @@
-import employees from "@/data/employees";
+import getAll from "@/data/employees";
 import type { Employee } from "@/types/employee";
 import { defineStore } from "pinia";
 
 export const useEmployeeStore = defineStore('employee', {
   actions: {
-    getEmployees() {
-      return new Promise<Employee[]>((resolve) => {
-        setTimeout(() => {
-          resolve(employees);
-        }, 2000);
-      })
+    async getEmployees() {    
+      return await getAll();
     },
   },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslintPlugin()],
-  server: {
-    port: 8080,
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
